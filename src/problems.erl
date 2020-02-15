d344ed939ea9c9dca3fdab00a68faf004fbec725
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-15 12:38:08 +0100

    add resolution for 2nd problems

--- src/problems.erl
+++ src/problems.erl
@@ -1,7 +1,8 @@
 -module(problems).
 
--export([last/1]).
+-export([last/1, last_but_last/1]).
 
+%% 1 %%
 
 last([]) ->
     nil;
@@ -9,3 +10,12 @@ last([Elem]) ->
     Elem;
 last([_|Tail]) ->
     last(Tail).
+
+%% 2 %%
+
+last_but_last([]) ->
+  nil;
+last_but_last([Head|[_]]) ->
+    Head;
+last_but_last([_|Tail]) ->
+  last_but_last(Tail).
